80c75ab95b6675883c9a0f8c2909307bc20589a3
Saikat Dey <saikatdey9735@gmail.com>
2021-09-05 21:00:35 +0530

    check_login not found

--- include/login.inc.php
+++ include/login.inc.php
@@ -1,4 +1,21 @@
 <?php
 if (isset($_POST['submit'])) {
-    echo 'this is working';
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    require_once('functions.inc.php');
+
+    $con = connect_db();
+
+    //Now follow the codes of the project i provided....
+
+    if ($rec = check_login($con, $email, $password)) {
+        //Store the email to SESSION.
+        session_start();
+        $_SESSION['login_email'] = $email;
+
+        header('location:../profile.php');
+    } else {
+        header('location:../login.php?error=invalidlogin');
+    }
 }
